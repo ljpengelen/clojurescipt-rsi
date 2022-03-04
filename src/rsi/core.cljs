da9e3d43c4a4cd53c69111368f043c5ab2774bc1
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2022-03-04 23:41:00 +0100

    Document and extend a little

--- src/rsi/core.cljs
+++ src/rsi/core.cljs
@@ -1,66 +1,132 @@
 (ns rsi.core
-  (:require [reagent.core :as r]
+  (:require [flatland.ordered.map :refer [ordered-map]]
+            [reagent.core :as r]
             [reagent.dom :as d]))
 
-(defonce circles (r/atom {}))
+;; The state of the app
+
+(defonce circles (r/atom (ordered-map)))
 (defonce mode (r/atom :recolor))
 
-(defn circle
+(comment
+  @circles
+  @mode
+  (reset! circles {}))
+
+;; Functions to create circles and colors
+
+(defn
+  circle
+  "Returns a map representing a circle with center (x, y),
+   color c, and radius r.
+   Radius r is optional and defaults to 10."
   ([x y c] (circle x y c 10))
   ([x y c r] {:x x :y y :c c :r r}))
 
 (comment
-  (reset! circles {}))
+  (circle 10 20 "#fff" 30))
+
+(defn random-color
+  "Returns a random color in hexadecimal notation."
+  [] (str "#" (.toString (rand-int 16777215) 16)))
+
+(comment
+  (random-color))
+
+;; Functions to manipulate circles
 
 (defn grow [{:keys [x y c r]}]
-  (circle x y c (* 2 r)))
+  (circle x y c (* 1.1 r)))
+
+(comment
+  (grow (circle 123 456 "#ffff00" 1)))
 
 (defn shrink [circle]
-  (update circle :r #(/ % 2)))
+  (update circle :r #(* 0.9 %)))
 
-(defn random-color []
-  (str "#" (.toString (rand-int 16777215) 16)))
+(comment
+  (shrink (circle 123 456 "#fff" 4)))
 
 (defn recolor [c]
   (assoc c :c (random-color)))
 
+(comment
+  (recolor (circle 123 456 "#fff")))
+
 (defn recolor-all [cs]
-  (reduce-kv (fn [m k v] (assoc m k (recolor v))) {} cs))
+  (reduce-kv (fn [m k v] (assoc m k (recolor v))) (ordered-map) cs))
 
 (comment
-  (random-color)
-  (circle 123 456 "#ff00ff")
-  (grow (circle 123 456 "#ffff00"))
-  (shrink (circle 123 456 "#fff"))
-  (recolor (circle 123 456 "#fff"))
   (recolor-all {"abc" (circle 123 456 "#fff")
                 "def" (circle 123 456 "#fff")}))
 
-(defn svg-circle [{:keys [x y r c]} on-click]
+;; Reagent components
+
+(defn svg-circle
+  "Takes a map of a circle and an on-click function,
+   and returns an SVG circle."
+  [{:keys [x y r c]} on-click]
   [:circle {:cx x
             :cy y
             :r r
             :fill c
             :on-click on-click}])
 
-(defn add-circle! [x y]
-  (let [color (random-color)
-        new-circle (circle x y color)]
-    (swap! circles assoc color new-circle)))
+(comment
+  (svg-circle (circle 12 34 "#00f") #()))
+
+(defn change-mode-button [current-mode new-mode label]
+  (let [style (when (= current-mode new-mode) {:font-weight "bold"})]
+    [:button {:on-click #(reset! mode new-mode)
+              :style style} label]))
+
+(comment
+  (change-mode-button :recolor :recolor "Change color")
+  (change-mode-button :recolor :shrink "Shrink"))
+
+;; Functions to manipulate the global state
+
+(defn add-circle!
+  ([x y] (add-circle! x y (random-color)))
+  ([x y c]
+   (let [new-circle (circle x y c)]
+     (swap! circles assoc c new-circle))))
+
+(comment
+  (add-circle! (rand-int 500) (rand-int 500))
+  (swap! circles recolor-all))
 
 (def modification-functions {:recolor recolor
-                            :grow grow
-                            :shrink shrink})
+                             :grow grow
+                             :shrink shrink})
 
 (defn modify-circle! [key]
   (let [f (get modification-functions @mode)]
     (swap! circles update key f)))
 
-(defn coords [e]
+(comment
+  (add-circle! (rand-int 500) (rand-int 500) "#00f")
+  (do
+    (reset! mode :recolor)
+    (modify-circle! "#00f"))
+  (do
+    (reset! mode :grow)
+    (modify-circle! "#00f"))
+  (do
+    (reset! mode :shrink)
+    (modify-circle! "#00f")))
+
+;; Utility function
+
+(defn coords
+  "Returns the coordinates of e relative to its parent."
+  [e]
   (let [rect (.. e -target getBoundingClientRect)
         x (- (.-clientX e) (.-left rect))
         y (- (.-clientY e) (.-top rect))]
     [x y]))
+
+;; Main app component
 
 (defn app []
   [:div
@@ -69,12 +135,17 @@
           :on-click (fn [e] (let [[x y] (coords e)] (add-circle! x y)))}
     (for [[key circle] @circles]
       ^{:key key} [svg-circle circle (fn [e] (.stopPropagation e) (modify-circle! key))])]
-   [:button {:on-click #(reset! mode :recolor)} "Change color"]
-   [:button {:on-click #(reset! mode :grow)} "Grow"]
-   [:button {:on-click #(reset! mode :shrink)} "Shrink"]])
+   [change-mode-button @mode :recolor "Change color"]
+   [change-mode-button @mode :grow "Grow"]
+   [change-mode-button @mode :shrink "Shrink"]
+   [:button {:on-click #(swap! circles recolor-all)} "Change all colors"]])
+
+;; Attach the main Reagent component to a DOM element
 
 (defn mount-root []
   (d/render [app] (.getElementById js/document "app")))
+
+;; Initialization at the start of the app
 
 (defn ^:export init! []
   (mount-root))
