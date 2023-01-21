b96572fe866e86463e59c8699b78e670f57d3cc9
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-21 13:47:10 +0100

    Use fragment identifier for navigation

--- src/rsi/core.cljs
+++ src/rsi/core.cljs
@@ -1,10 +1,23 @@
 (ns rsi.core
-  (:require [reagent.dom :as d]
+  (:require [reagent.core :as r]
+            [reagent.dom :as d]
             [rsi.basics :as basics]
             [rsi.largest :as largest]
             [rsi.multiplication-tables :as multiplication-tables]
             [rsi.paint :as paint]
             [rsi.second-largest :as second-largest]))
+
+;; Store and update the current fragment identifier
+
+(defonce fragment-identifier (r/atom js/window.location.hash))
+
+(defn handle-hash-change! [_]
+  (js/console.log "hash changed")
+  (reset! fragment-identifier js/window.location.hash))
+
+(defn handle-hash-changes! []
+  (js/console.log "listening to hash changes")
+  (js/addEventListener "hashchange" handle-hash-change!))
 
 ;; Reagent components
 
@@ -13,33 +26,30 @@
 
 (defn app-selector []
   [:ul
-   [app-link "basics" "Reagent Basics"]
-   [app-link "largest" "Click the largest circle"]
-   [app-link "multiplication-tables" "Multiplication tables"]
-   [app-link "paint" "Paint"]
-   [app-link "second-largest" "Click the second largest circle"]])
+   [app-link "#/basics" "Reagent Basics"]
+   [app-link "#/largest" "Click the largest circle"]
+   [app-link "#/multiplication-tables" "Multiplication tables"]
+   [app-link "#/paint" "Paint"]
+   [app-link "#/second-largest" "Click the second largest circle"]])
 
 ;; Main app component
 
-;; A true SPA would perform navigation without triggering
-;; page reloads. This simple approach is good enough
-;; for this app.
-
-(defn app []
-  (case js/window.location.pathname
-    "/basics" [basics/app]
-    "/largest" [largest/app]
-    "/multiplication-tables" [multiplication-tables/app]
-    "/paint" [paint/app]
-    "/second-largest" [second-largest/app]
+(defn app [hash]
+  (case @hash
+    "#/basics" [basics/app]
+    "#/largest" [largest/app]
+    "#/multiplication-tables" [multiplication-tables/app]
+    "#/paint" [paint/app]
+    "#/second-largest" [second-largest/app]
     [app-selector]))
 
 ;; Attach the main app component to a DOM element
 
 (defn mount-root []
-  (d/render [app] (.getElementById js/document "app")))
+  (d/render [app fragment-identifier] (.getElementById js/document "app")))
 
 ;; Initialization at the start of the app
 
 (defn ^:export init! []
+  (handle-hash-changes!)
   (mount-root))
