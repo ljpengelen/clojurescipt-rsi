fe9abb01d4c4098cf09867b5cc2d5770f5ba47d9
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-21 21:08:40 +0100

    Add rehearsal phase

--- src/rsi/multiplication_tables.cljs
+++ src/rsi/multiplication_tables.cljs
@@ -1,6 +1,18 @@
 (ns rsi.multiplication-tables
   (:require [reagent.core :as r]))
 
+;; Utilities
+
+(defonce timeout-id (r/atom nil))
+
+(defn set-timeout! [f]
+  (reset! timeout-id (js/setTimeout f 4000)))
+
+(defn clear-timeout! []
+  (js/clearTimeout @timeout-id))
+
+;; State
+
 (defn random-number []
   (inc (rand-int 10)))
 
@@ -10,31 +22,53 @@
 (defonce left (r/atom (random-number)))
 (defonce right (r/atom (random-number)))
 
+(defonce deadline-passed? (r/atom false))
+
+(defonce wrong-answers (r/atom #{}))
+
+(defonce mode (r/atom :against-the-clock))
+
+(defn set-deadline! []
+  (set-timeout! (fn [] (reset! deadline-passed? true))))
+
 (defn new-numbers! []
-  (reset! left (random-number))
-  (reset! right (random-number)))
+  (reset! deadline-passed? false)
+  (when (>= (count @wrong-answers) 5)
+    (reset! mode :repeat-wrong-answers))
+  (when (empty? @wrong-answers)
+    (reset! mode :against-the-clock))
+  (if (= @mode :against-the-clock)
+    (do
+      (reset! left (random-number))
+      (reset! right (random-number))
+      (set-deadline!))
+    (let [[new-left new-right] (first @wrong-answers)]
+      (reset! left new-left)
+      (reset! right new-right))))
 
-(defn win! []
+(defn right-anwser! []
+  (swap! wrong-answers disj [@left @right])
   (swap! score inc)
-  (swap! highscore max @score)
-  (new-numbers!))
+  (swap! highscore max @score))
 
-(defn lose! []
+(defn wrong-answer! []
   (reset! score 0)
-  (new-numbers!))
+  (swap! wrong-answers conj [@left @right]))
 
 (comment
-  (win!)
-  (lose!)
+  (right-anwser!)
+  (wrong-answer!)
   (new-numbers!))
+
+;; Reagent components
 
 (defn score-view [label score]
   [:div (str label ": " score)])
 
 (defn question-view []
   (let [value (r/atom "")]
-    (fn [left right on-submit]
-      [:div (str left " x " right " = ")
+    (fn [left right deadline-passed? on-submit]
+      [:div (when deadline-passed? {:class "deadline-passed"}) (str left " x " right " = ")
        [:form {:on-submit (fn [_]
                             (on-submit @value)
                             (reset! value ""))}
@@ -43,12 +77,31 @@
                  :on-change (fn [e]
                               (reset! value (.. e -target -value)))}]]])))
 
+(defn wrong-answer-view [wrong-answers]
+  [:ul
+   (for [[left right :as answer] wrong-answers]
+     ^{:key answer} [:li left " x " right])])
+
+;; A Form-3 Reagent component is needed here because we
+;; need to start the game loop when the component mounts
+;; and stop it when it unmounts
+
+;; https://github.com/reagent-project/reagent/blob/master/doc/CreatingReagentComponents.md
+
 (defn app []
-  [:div.app
-   [score-view "Score" @score]
-   [score-view "High score" @highscore]
-   [question-view @left @right
-    (fn [answer]
-      (if (= (str (* @left @right)) answer)
-        (win!)
-        (lose!)))]])
+  (r/create-class
+   {:component-did-mount set-deadline!
+    :component-will-unmount clear-timeout!
+    :reagent-render
+    (fn []
+      [:div.app
+       [score-view "Score" @score]
+       [score-view "High score" @highscore]
+       [question-view @left @right @deadline-passed?
+        (fn [answer]
+          (clear-timeout!)
+          (if (and (not @deadline-passed?) (= (str (* @left @right)) answer))
+            (right-anwser!)
+            (wrong-answer!))
+          (new-numbers!))]
+       [wrong-answer-view @wrong-answers]])}))
