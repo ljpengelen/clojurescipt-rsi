b0994cc2eab7ab470d1aa189b792c14001afe65c
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-22 14:43:53 +0100

    Start by answering first question

--- src/rsi/multiplication_tables.cljs
+++ src/rsi/multiplication_tables.cljs
@@ -84,26 +84,15 @@
    (for [[left right :as answer] wrong-answers]
      ^{:key answer} [:li left " x " right])])
 
-;; A Form-3 Reagent component is needed here because we
-;; need to start the game loop when the component mounts
-;; and stop it when it unmounts
-
-;; https://github.com/reagent-project/reagent/blob/master/doc/CreatingReagentComponents.md
-
 (defn app []
-  (r/create-class
-   {:component-did-mount set-deadline!
-    :component-will-unmount clear-timeout!
-    :reagent-render
-    (fn []
-      [:div.app
-       [score-view "Score" @score]
-       [score-view "High score" @highscore]
-       [question-view @left @right @deadline-passed?
-        (fn [answer]
-          (clear-timeout!)
-          (if (and (not @deadline-passed?) (= (str (* @left @right)) answer))
-            (right-anwser!)
-            (wrong-answer!))
-          (new-numbers!))]
-       [wrong-answer-view @wrong-answers]])}))
+  [:div.app
+   [score-view "Score" @score]
+   [score-view "High score" @highscore]
+   [question-view @left @right @deadline-passed?
+    (fn [answer]
+      (clear-timeout!)
+      (if (and (not @deadline-passed?) (= (str (* @left @right)) answer))
+        (right-anwser!)
+        (wrong-answer!))
+      (new-numbers!))]
+   [wrong-answer-view @wrong-answers]])
