f2b4a8468fc9bbed449a10384c2f9d6b1f32dc0c
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2023-01-24 22:45:57 +0100

    Test transformation of state

--- src/test/rsi/multiplication_tables_test.cljs
+++ src/test/rsi/multiplication_tables_test.cljs
@@ -1,6 +1,7 @@
 (ns rsi.multiplication-tables-test
   (:require [cljs.test :refer (deftest is testing)]
-            [rsi.multiplication-tables :refer [update-highscore update-score]]))
+            [rsi.multiplication-tables :refer [process-answer update-highscore
+                                               update-score]]))
 
 (deftest score
   (testing "increments given correct answer in time"
@@ -26,3 +27,23 @@
         :highscore 3}
        (update-highscore {:score 3
                           :highscore 2}))))
+
+(deftest transforming-state
+  (testing "correct answer"
+    (is (= {:question [1 2]
+            :score 1
+            :highscore 1
+            :mode :against-the-clock
+            :deadline-passed? false}
+           (process-answer {:question [2 3]} "6" [1 2]))))
+   (testing "wrong answer"
+     (is (= {:question [2 3]
+             :score 0
+             :highscore 10
+             :mode :correct-current-question
+             :wrongly-answered #{[2 3]}
+             :deadline-passed? false}
+            (process-answer {:question [2 3]
+                             :score 5
+                             :highscore 10
+                             :wrongly-answered #{}} "5" [1 2])))))
